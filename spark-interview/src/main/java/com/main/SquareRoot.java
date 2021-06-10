b581869b37de028921541e5143c71198b036c10b
HP <HP@192.168.0.5>
2021-06-10 18:21:12 +0530

    adding code comments and checks

--- spark-interview/src/main/java/com/main/SquareRoot.java
+++ spark-interview/src/main/java/com/main/SquareRoot.java
@@ -1,5 +1,6 @@
 package com.main;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
@@ -9,15 +10,24 @@ import org.apache.spark.sql.SparkSession;
 public class SquareRoot {
 
 	public static void main(String[] args) {
+		
+		/* creating saprksession object */
 		SparkSession spark = SparkSession.builder().appName("sqrt").master("local").getOrCreate();
 
-		Dataset<Row> df = spark.read()
-				.text("F:\\work\\canada\\java-scala-wspace\\spark-interview\\src\\main\\resources\\data.txt");
+		/* Reading data from text file and creating a dataset of string values */ 
+		Dataset<String> df = spark.read().textFile("C:\\Users\\HP\\git\\repository\\spark-interview\\src\\main\\resources\\data.txt");
 
-		Dataset<Integer> fd1 = df.map((MapFunction<Row, Integer>) row -> Integer.parseInt(row.getString(0))
-				* Integer.parseInt(row.getString(0)), Encoders.INT());
+		/* converting dataset of string to dataset of its squre value in long*/ 
+		Dataset<Long> fd1 = df.map((MapFunction<String, Long>) row -> 
+		{
+			long sqr=0;
+			if(StringUtils.isNumeric(row))
+				sqr = Long.parseLong(row)* Long.parseLong(row);
+			return sqr;
+		}, Encoders.LONG());
 
-		int sum = fd1.reduce((a, b) -> a + b);
+		/* summation of all the values in dataset */
+		long sum = fd1.reduce((a, b) -> a + b);
 
 		System.out.println(Math.sqrt(sum));
 	}
