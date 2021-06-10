33656ac2722c017f0ebada52b28b345f36c0b8c5
HP <HP@192.168.0.5>
2021-06-10 19:28:22 +0530

    changes

--- spark-interview/src/main/java/com/main/Permutation.java
+++ spark-interview/src/main/java/com/main/Permutation.java
@@ -1,27 +1,24 @@
 package com.main;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Permutation {
 	
-	public static List<int[]> list;
-	//public static int count = 0;
+	public static List<List<Integer>> list;
 	
 	public static void permute(int[] arr, int current_index)
 	{
 		if(current_index == arr.length)
 		{
-			int[] combination = new int[arr.length];
+			List<Integer> combination = new ArrayList<Integer>();
 			for(int j=0; j< arr.length; j++)
 			{
-				combination[j]=arr[j];
+				combination.add(j,arr[j]);
 			}
 			
-			//list[count]=combination;
 			list.add(combination);
-			//count++;
+			
 		}
 		else
 		{
@@ -45,29 +42,14 @@ public class Permutation {
 
 		int[] arr = { 1, 2, 3, 4 };
 		
-		/*
-		 * int factorial = 1;
-		 * 
-		 * for(int n=1;n<=arr.length;n++) { factorial = factorial*n; }
-		 */
-		
-		//list = new int[factorial][];
-		list = new ArrayList<int[]>();
+		list = new ArrayList<List<Integer>>();
 		
 		if(arr.length!=0)
 		{
 			permute(arr, 0);
 		}
 		
-		System.out.print("[");
-		
-		
-		
-		for(int[] a: list)
-		{
-			System.out.print(Arrays.toString(a)+",");
-		}
-		System.out.print("]");
+		System.out.println(list);
 	}
 
 }
